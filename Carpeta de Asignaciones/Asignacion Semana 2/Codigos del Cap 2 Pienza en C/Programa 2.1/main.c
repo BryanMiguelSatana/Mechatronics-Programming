84ea61ffdf2275e6f30d8948c86020f46e66cce2
BryanMiguelSatana <20240702@itla.edu.do>
2024-05-22 19:53:37 -0400

    Add files via upload

---
+++ Carpeta de Asignaciones/Asignacion Semana 2/Codigos del Cap 2 Pienza en C/Programa 2.1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float PRO;
+    printf("Ingrese el promedio del alumno: ");
+    scanf("%f", &PRO);
+    if (PRO >= 6)
+    printf("\nAprobado");
+
+}
---
+++ Carpeta de Asignaciones/Asignacion Semana 2/Codigos del Cap 2 Pienza en C/Programa 2.10/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int NUM;
+    printf("Ingrese el numero: ");
+    scanf("%d", &NUM);
+    if (NUM == 0)
+       printf("\nNulo");
+   else
+       if (pow (-1, NUM) > 0)
+        printf("\nPar");
+       else
+           printf("\nImpar");
+}
